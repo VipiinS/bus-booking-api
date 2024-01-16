e9da5515e271aab990702fb6650f2974af0faaea
VipiinS <Vipinshanmugam@gmail.com>
2024-01-16 15:16:13 +0530

    Changed User to UserDetails adn added a Enum Role

--- src/main/java/com/example/busticketbooking/busticketbookingapi/entity/User.java
+++ src/main/java/com/example/busticketbooking/busticketbookingapi/entity/User.java
@@ -6,7 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Data
@@ -15,17 +19,53 @@ import java.util.List;
 @Entity
 @Builder
 @Table(name = "user_")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
+    private String Username;
     private String firstName;
     private String lastName;
-
     @Column(unique = true)
     private String email;
     private String password;
     @OneToMany(mappedBy = "user")
     private List<Booking> bookings;
+    private boolean isAccountNonExpired;
+    private boolean isAccountNonLocked;
+    private boolean isCredentialsNonExpired;
+    private boolean isEnabled;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.Username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
